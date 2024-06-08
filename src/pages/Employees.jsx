4cde9a79a69f8976e4cfad2b2389ae9cb413dac0
kinjal-jn <kinjaljain.cse25@jecrc.ac.in>
2024-06-08 23:40:23 +0530

    final commit

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page } from '@syncfusion/ej2-react-grids';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, Search, Page, Toolbar } from '@syncfusion/ej2-react-grids';
 
 import { employeesData, employeesGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Employees = () => {
-  const toolbarOptions = ['Search'];
-
-  const editing = { allowDeleting: true, allowEditing: true };
-
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Employees" />
@@ -17,15 +13,12 @@ const Employees = () => {
         width="auto"
         allowPaging
         allowSorting
-        pageSettings={{ pageCount: 5 }}
-        editSettings={editing}
-        toolbar={toolbarOptions}
+        toolbar={['Search']}
       >
         <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
-        <Inject services={[Search, Page]} />
+        <Inject services={[Search, Page, Toolbar]} />
 
       </GridComponent>
     </div>
